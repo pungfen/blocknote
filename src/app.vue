8df5497540579e82875acac18ebf134be5198093
peng <fp94214@gmail.com>
2026-02-09 19:19:14 +0800

    xx

--- src/app.vue
+++ src/app.vue
@@ -1,3 +1,9 @@
+<script setup lang="ts">
+const videoUrl = ref(
+  'https://resource-enocboot.enoch-car.com/tenant/37786/1770623556159/%E7%BB%B4%E4%BF%AE%E8%AE%B0%E5%BD%95.mp4'
+)
+</script>
+
 <template>
-  <BlockNoteEditor />
+  <BlockNoteEditor :editable="false" />
 </template>
--- src/components/BlockNoteEditor.vue
+++ src/components/BlockNoteEditor.vue
@@ -4,22 +4,85 @@ import { setVeauryOptions } from 'veaury'
 
 import { createRoot } from 'react-dom/client'
 
-import { BlockNoteEditor } from '@blocknote/core'
+import {
+  BlockNoteEditor,
+  BlockNoteSchema,
+  createBlockSpec,
+  defaultBlockSpecs,
+  defaultProps,
+  createVideoBlockConfig,
+  type BlockSchema,
+  type BlockNoteEditorOptions,
+  type InlineContentSchema,
+  type StyleSchema,
+  FILE_VIDEO_ICON_SVG
+} from '@blocknote/core'
+
+import { ResizableFileBlockWrapper, createReactBlockSpec } from '@blocknote/react'
+
 import { BlockNoteView } from '@blocknote/mantine'
 
 import { zh } from '@blocknote/core/locales'
 
+type BlockNoteEditorProps = BlockNoteEditorOptions<BlockSchema, InlineContentSchema, StyleSchema>
+
 setVeauryOptions({ react: { createRoot } })
 
 const { theme = 'light', onUploadFile } = defineProps<{
+  editable?: boolean
   theme?: 'light' | 'dark'
   onUploadFile?: (file: File) => Promise<string>
 }>()
+const NoteView = applyPureReactInVue(BlockNoteView)
+
 const html = defineModel<string>('html')
 
 const editor = shallowRef<BlockNoteEditor | null>(null)
 
-const NoteView = applyPureReactInVue(BlockNoteView)
+const createVideoBlockSpec = createBlockSpec(
+  {
+    type: 'video',
+    content: 'none',
+    propSchema: {
+      textAlignment: defaultProps.textAlignment,
+      backgroundColor: defaultProps.backgroundColor,
+      name: { default: '' },
+      url: { default: '' },
+      caption: { default: '' },
+      showPreview: { default: true },
+      previewWidth: { default: undefined, type: 'number' }
+    }
+  },
+  {
+    render(block, editor) {
+      const icon = document.createElement('div')
+      icon.innerHTML = FILE_VIDEO_ICON_SVG
+
+      const videoWrapper = document.createElement('div')
+      videoWrapper.className = 'bn-visual-media-wrapper'
+
+      const video = document.createElement('video')
+      video.className = 'bn-visual-media'
+      if (editor.resolveFileUrl) {
+        editor.resolveFileUrl(block.props.url).then((downloadUrl) => {
+          video.src = downloadUrl
+        })
+      } else {
+        video.src = block.props.url
+      }
+
+      video.controls = true
+      video.contentEditable = 'false'
+      video.draggable = false
+      video.width = block.props.previewWidth
+      videoWrapper.appendChild(video)
+
+      return {}
+    }
+  }
+)
+
+const VideoBlock = createReactBlockSpec(createVideoBlockConfig, (config) => ({}))
 
 onMounted(() => {
   editor.value = BlockNoteEditor.create({
@@ -47,8 +110,23 @@ defineExpose({
   redo: () => editor.value?.redo(),
   focus: () => editor.value?.focus()
 })
+
+const editorProps = {
+  schema: BlockNoteSchema.create({
+    blockSpecs: {
+      ...defaultBlockSpecs,
+      video: createVideoBlockSpec()
+    }
+  })
+}
 </script>
 
 <template>
-  <NoteView v-if="editor?.mount" :theme="theme" :editor="editor" @change="change" />
+  <NoteView
+    v-if="editor?.mount"
+    :theme="theme"
+    :editor="editor"
+    :editor-props="editorProps"
+    @change="change"
+  />
 </template>
